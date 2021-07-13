79204f2f027bc04de1ddeb6071665b10a7ba3f96
ekiryakov <ekiryakov@gmail.com>
2021-07-14 00:41:45 +0300

    Vendor Auth (test)

--- src/Security/VendorAuthenticator.php
+++ src/Security/VendorAuthenticator.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -50,9 +51,13 @@ class VendorAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_homepage'));
+    }
+
+    public function checkCredentials($credentials, UserInterface $user): bool
+    {
+        //return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+        return $credentials['password'] === 'password';
     }
 
     protected function getLoginUrl(Request $request): string
