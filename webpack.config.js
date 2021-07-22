e2415321bb117df8ecc51b301e9e69aa51330421
ekiryakov <ekiryakov@gmail.com>
2021-07-22 09:55:00 +0300

    webpack fix#2

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
-// if (!Encore.isRuntimeEnvironmentConfigured()) {
-//     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
-// }
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
 
 Encore
     // directory where compiled assets will be stored
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .enableVueLoader()
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     // .enableStimulusBridge('./assets/controllers.json')
