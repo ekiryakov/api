0701c34b26f8a48466da5c3d6ad2fb18a4ce26a1
ekiryakov <ekiryakov@gmail.com>
2021-07-22 09:02:31 +0300

    vuetify template

--- assets/app.js
+++ assets/app.js
@@ -3,4 +3,9 @@ import vuetify from './plugins/vuetify.js' // path to vuetify export
 
 new Vue({
     vuetify,
+    template: '<v-app>' +
+            '<v-main>' +
+                '<v-container>Hello world</v-container>' +
+            '</v-main>' +
+        '</v-app>'
 }).$mount('#app')
