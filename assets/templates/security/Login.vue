b06856a1559ac030485ba6152853ee76660336dd
ekiryakov <ekiryakov@gmail.com>
2021-08-02 01:57:31 +0300

    new login component (vue)

--- assets/templates/security/Login.vue
+++ assets/templates/security/Login.vue
@@ -1,98 +1,70 @@
 <template>
-  <v-form
-      ref="form"
-      v-model="valid"
-      lazy-validation
-  >
+  <v-form ref="form" v-model="valid" lazy-validation>
+
+    <h1>Please sign in</h1>
+
     <v-text-field
-        v-model="name"
-        :counter="10"
-        :rules="nameRules"
-        label="Name"
-        required
-    ></v-text-field>
+        name="_csrf_token"
+        :value="csrf_token"
+        type="hidden">
+    </v-text-field>
 
     <v-text-field
         v-model="email"
         :rules="emailRules"
-        label="E-mail"
-        required
-    ></v-text-field>
+        :value="last_username"
+        label="Email"
+        required>
+    </v-text-field>
 
-    <v-select
-        v-model="select"
-        :items="items"
-        :rules="[v => !!v || 'Item is required']"
-        label="Item"
-        required
-    ></v-select>
+    <v-text-field
+        v-model="password"
+        :counter="10"
+        :rules="passwordRules"
+        label="Password"
+        required>
+    </v-text-field>
 
     <v-checkbox
-        v-model="checkbox"
-        :rules="[v => !!v || 'You must agree to continue!']"
-        label="Do you agree?"
-        required
-    ></v-checkbox>
+        v-model="remember_me"
+        name="_remember_me"
+        label="Do you agree?">
+    </v-checkbox>
 
     <v-btn
         :disabled="!valid"
         color="success"
-        class="mr-4"
-        @click="validate"
-    >
-      Validate
+        @click="validate">
+      Sign in
     </v-btn>
 
-    <v-btn
-        color="error"
-        class="mr-4"
-        @click="reset"
-    >
-      Reset Form
-    </v-btn>
-
-    <v-btn
-        color="warning"
-        @click="resetValidation"
-    >
-      Reset Validation
-    </v-btn>
   </v-form>
 </template>
 
 <script>
 export default {
+  props: {
+    csrf_token: String,
+    last_username: String,
+  },
   data: () => ({
     valid: true,
-    name: '',
-    nameRules: [
-      v => !!v || 'Name is required',
-      v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+    password: '',
+    passwordRules: [
+      v => !!v || 'Password is required',
+      v => (v && v.length <= 10) || 'Password must be less than 10 characters',
     ],
     email: '',
     emailRules: [
       v => !!v || 'E-mail is required',
       v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
     ],
-    select: null,
-    items: [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4',
-    ],
-    checkbox: false,
+    remember_me: true,
   }),
 
   methods: {
     validate () {
       this.$refs.form.validate()
-    },
-    reset () {
-      this.$refs.form.reset()
-    },
-    resetValidation () {
-      this.$refs.form.resetValidation()
     },
   },
 }
