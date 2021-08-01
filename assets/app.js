ed7409a72b87456716d5cf44d86a0a947c8a871a
ekiryakov <ekiryakov@gmail.com>
2021-08-01 17:18:47 +0300

    new way

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import vuetify from './plugins/vuetify.js' // path to vuetify export
 
+import Login from "./templates/security/Login";
+
 new Vue({
     vuetify,
+    component: {
+        Login
+    }
 }).$mount('#app')
