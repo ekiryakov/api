a9a8b886fac88f31ee6111c1cfbc93245408af00
ekiryakov <ekiryakov@gmail.com>
2021-08-09 22:41:42 +0300

    template style #13

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import vuetify from './plugins/vuetify.js' // path to vuetify export
 
+import './styles/app.scss';
+
 import Login from "./templates/security/Login";
 
 new Vue({
