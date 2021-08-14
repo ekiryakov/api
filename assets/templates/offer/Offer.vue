e65b51ef5423104f2e2330ad5d4e29e5f659af0c
ekiryakov <ekiryakov@gmail.com>
2021-08-14 15:10:54 +0300

    offers (test serialize) #14

--- assets/templates/offer/Offer.vue
+++ assets/templates/offer/Offer.vue
@@ -1,17 +1,15 @@
 <template>
   <v-list-item :key="offer.title">
-    <template v-slot:default="{ active }">
-      <v-list-item-content>
-        <v-list-item-title v-text="offer.title"></v-list-item-title>
-        <v-list-item-subtitle class="text--primary" v-text="offer.cost"></v-list-item-subtitle>
-        <v-list-item-subtitle v-text="offer.description"></v-list-item-subtitle>
-      </v-list-item-content>
-      <v-list-item-action>
-        <v-list-item-action-text>Action</v-list-item-action-text>
-        <v-btn :href="show"></v-btn>
-        <v-btn :href="edit"></v-btn>
-      </v-list-item-action>
-    </template>
+    <v-list-item-content>
+      <v-list-item-title v-text="offer.title"></v-list-item-title>
+      <v-list-item-subtitle class="text--primary" v-text="offer.cost"></v-list-item-subtitle>
+      <v-list-item-subtitle v-text="offer.description"></v-list-item-subtitle>
+    </v-list-item-content>
+    <v-list-item-action>
+      <v-list-item-action-text>Action</v-list-item-action-text>
+      <v-btn :href="show"></v-btn>
+      <v-btn :href="edit"></v-btn>
+    </v-list-item-action>
   </v-list-item>
 </template>
 
