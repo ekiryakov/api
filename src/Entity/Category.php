08801a3a6cb2ec2ca2949a89558dd5b54c6b5d0b
ekiryakov <ekiryakov@gmail.com>
2021-08-16 11:15:57 +0300

    item list #55

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,6 +6,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Serializer\Annotation\Ignore;
 
 /**
@@ -17,26 +18,31 @@ class Category
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"category_offer"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"category_offer"})
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"category_offer"})
      */
     private $image;
 
     /**
      * @ORM\Column(type="text", nullable=true)
+     * @Groups({"category_offer"})
      */
     private $description;
 
     /**
      * @ORM\OneToMany(targetEntity=Offer::class, mappedBy="category")
+     * @Ignore
      */
     private $offers;
 
