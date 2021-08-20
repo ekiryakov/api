68f142c071b73c59908668b1aac270f4d0346cea
ekiryakov <ekiryakov@gmail.com>
2021-08-21 02:24:34 +0300

    Customer role #13

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,7 @@ import vuetify from './plugins/vuetify.js' // path to vuetify export
 import './styles/app.scss';
 
 import Login from "./templates/security/Login";
+import CustomerLogin from "./templates/security/CustomerLogin";
 import Offer from "./templates/offer/Offer";
 import Categories from "./templates/offer/Categories";
 
@@ -12,6 +13,7 @@ new Vue({
     delimiters: ['${', '}$'],
     components: {
         Login,
+        CustomerLogin,
         Offer,
         Categories,
     },
