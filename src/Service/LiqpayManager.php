4e4560ced4cd85ee4019f87cb0125222a64c6f67
ekiryakov <ekiryakov@gmail.com>
2021-08-26 00:49:47 +0300

    Liqpay #27

--- src/Service/LiqpayManager.php
+++ src/Service/LiqpayManager.php
@@ -110,7 +110,7 @@ class LiqpayManager implements PaymentManagerInterface
     {
         $description = 'Subscription #' . $subscription->getId() . ':' . PHP_EOL;
         foreach ($subscription->getOffer() as $offer) {
-            $description .= '&check;' . $offer->getTitle() . PHP_EOL;
+            $description .= '\u2713' . $offer->getTitle() . PHP_EOL;
         }
 
         return $description;
