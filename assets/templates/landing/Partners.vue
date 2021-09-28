444ed05157a34db53d5247e47ec9eb2be0465dc3
ekiryakov <ekiryakov@gmail.com>
2021-09-28 14:53:40 +0300

    Landing page #87

--- assets/templates/landing/Partners.vue
+++ assets/templates/landing/Partners.vue
@@ -1,17 +1,11 @@
 <template>
-  <section>
-    <v-slide-group v-model="model" class="pa-4" active-class="success" show-arrows>
-      <v-slide-item v-for="n in 15" :key="n" v-slot="{ active, toggle }">
-        <v-card :color="active ? undefined : 'grey lighten-1'" class="ma-4" height="200" width="100" @click="toggle">
-          <v-row class="fill-height" align="center" justify="center">
-            <v-scale-transition>
-              <v-icon v-if="active" color="white" size="48" v-text="'mdi-close-circle-outline'"></v-icon>
-            </v-scale-transition>
-          </v-row>
-        </v-card>
-      </v-slide-item>
-    </v-slide-group>
-  </section>
+  <v-slide-group v-model="model" active-class="success" show-arrows>
+    <v-slide-item v-for="n in 15" :key="n" v-slot="{ active, toggle }">
+      <v-card :color="active ? undefined : 'grey lighten-1'" class="ma-4" height="200" width="100" @click="toggle">
+
+      </v-card>
+    </v-slide-item>
+  </v-slide-group>
 </template>
 
 <script>
