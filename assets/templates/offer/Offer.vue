516de2c80309a9746d1fd79fc00600902b0ac725
ekiryakov <ekiryakov@gmail.com>
2021-09-28 12:23:11 +0300

    Landing page #81

--- assets/templates/offer/Offer.vue
+++ assets/templates/offer/Offer.vue
@@ -1,39 +1,41 @@
 <template>
-  <v-list-item :key="data.title">
-    <v-list-item-content>
-      <v-list-item-title v-text="data.title"></v-list-item-title>
-      <v-list-item-subtitle v-text="data.cost" class="text--primary"></v-list-item-subtitle>
-      <v-list-item-subtitle v-text="data.description"></v-list-item-subtitle>
-    </v-list-item-content>
-    <v-list-item-action>
-      <v-menu bottom left rounded="lg">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-        <v-list nav width="200">
-          <v-list-item :href="show">
-            <v-list-item-icon>
-              <v-icon>mdi-eye</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>show</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item :href="edit">
-            <v-list-item-icon>
-              <v-icon>mdi-pencil</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>edit</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-list-item-action>
+  <div>
+    <v-list-item :key="data.title">
+      <v-list-item-content>
+        <v-list-item-title v-text="data.title"></v-list-item-title>
+        <v-list-item-subtitle v-text="data.cost" class="text--primary"></v-list-item-subtitle>
+        <v-list-item-subtitle v-text="data.description"></v-list-item-subtitle>
+      </v-list-item-content>
+      <v-list-item-action>
+        <v-menu bottom left rounded="lg">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+          <v-list nav width="200">
+            <v-list-item :href="show">
+              <v-list-item-icon>
+                <v-icon>mdi-eye</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>show</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item :href="edit">
+              <v-list-item-icon>
+                <v-icon>mdi-pencil</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>edit</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-list-item-action>
+    </v-list-item>
     <v-divider></v-divider>
-  </v-list-item>
+  </div>
 </template>
 
 <script>
