b542feaa54a977d63d1c51eed7cdab4891c8ab68
ekiryakov <ekiryakov@gmail.com>
2021-09-28 14:58:12 +0300

    Landing page #88

--- assets/templates/landing/Partners.vue
+++ assets/templates/landing/Partners.vue
@@ -1,9 +1,7 @@
 <template>
   <v-slide-group v-model="model" active-class="success" show-arrows>
     <v-slide-item v-for="n in 15" :key="n" v-slot="{ active, toggle }">
-      <v-card :color="active ? undefined : 'grey lighten-1'" class="ma-4" height="200" width="100" @click="toggle">
-
-      </v-card>
+      <v-card :color="'grey lighten-1'" height="200" width="200"></v-card>
     </v-slide-item>
   </v-slide-group>
 </template>
