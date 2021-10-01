af3b9d4d1223c24dd35551d386a01b76e0f08bd7
ekiryakov <ekiryakov@gmail.com>
2021-10-01 13:42:25 +0300

    Landing page (UserTabs) #4

--- assets/templates/landing/UserTabs.vue
+++ assets/templates/landing/UserTabs.vue
@@ -1,20 +1,24 @@
 <template>
   <section>
     <v-tabs v-model="user" centered>
-      <v-tab :href="`#customer`">
+      <v-tab href="#customer">
         customer
       </v-tab>
-      <v-tab :href="`#vendor`">
+      <v-tab href="#vendor">
         vendor
       </v-tab>
     </v-tabs>
     <v-tabs-items v-model="user">
-      <v-tab-item :value="`customer`">
-        customer content
+      <v-tab-item value="customer">
+        <p class="font-weight-light">
+          customer content
+        </p>
         <v-btn :href="customer_register" elevation="2" rounded x-large>Registration</v-btn>
       </v-tab-item>
-      <v-tab-item :value="`vendor`">
-        vendor content
+      <v-tab-item value="vendor">
+        <p class="font-weight-light">
+          vendor content
+        </p>
         <v-btn :href="vendor_register" elevation="2" rounded x-large>Registration</v-btn>
       </v-tab-item>
     </v-tabs-items>
