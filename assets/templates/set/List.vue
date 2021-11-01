65713069f70b5b9313b93c66c9baf30a7cb71829
ekiryakov <ekiryakov@gmail.com>
2021-11-01 16:09:33 +0200

    Landing page (Set integration) #9

--- assets/templates/set/List.vue
+++ assets/templates/set/List.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-slide-group center-active v-model="model" active-class="success">
+  <v-slide-group center-active v-model="model" active-class="success" show-arrows>
     <v-slide-item v-for="set in sets" :key="set.id" v-slot="{ active, toggle }">
       <Set :data="set"></Set>
     </v-slide-item>
