b6b534c60e50adcbc148a8b23e09569cacf4586b
ekiryakov <ekiryakov@gmail.com>
2021-11-04 17:15:50 +0200

    Customer register pages #32

--- assets/templates/registration/CustomerRegister.vue
+++ assets/templates/registration/CustomerRegister.vue
@@ -44,7 +44,7 @@
     </v-text-field>
     <v-checkbox
         v-model="agreeTerms"
-        :aria-checked="agreeTerms"
+        value="true"
         name="customer_registration_form[agreeTerms]"
         id="customer_registration_form_agreeTerms"
         label="Agree terms">
