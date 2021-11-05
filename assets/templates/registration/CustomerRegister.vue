94724b8714265429fc05f99c7da48e61ff71f21b
ekiryakov <ekiryakov@gmail.com>
2021-11-05 12:31:16 +0200

    Customer register pages #80

--- assets/templates/registration/CustomerRegister.vue
+++ assets/templates/registration/CustomerRegister.vue
@@ -44,7 +44,6 @@
     </v-text-field>
     <v-checkbox
         v-model="agreeTerms"
-        :value="this !== undefined ? this.agreeTerms : false"
         name="customer_registration_form[agreeTerms]"
         id="customer_registration_form_agreeTerms"
         label="Agree terms">
@@ -90,7 +89,7 @@ export default {
     phone_number: '',
     email: '',
     name: '',
-    agreeTerms: false,
+    agreeTerms: true,
   }),
   methods: {
     validate () {
